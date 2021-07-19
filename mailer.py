db9e6dd4242f2148aa93f61f0608b8e030ea9929
dhess <dan.hess@kippnorcal.org>
2021-07-19 10:58:52 -0500

    Add mailgun mailer class

--- mailer.py
+++ mailer.py
@@ -1,21 +1,54 @@
-import smtplib
+import json
 import os
 
+import requests
 
-GMAIL_USER = os.getenv("GMAIL_USER")
-GMAIL_PWD = os.getenv("GMAIL_PWD")
-SLACK_EMAIL = os.getenv("SLACK_EMAIL")
 
-def notify(school, count, error=False, error_message=None):
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login(GMAIL_USER, GMAIL_PWD)
-    if error:
-        subject = "Tableau Batch Print - Error"
-        text = f"The Student Profile Batch Printer encountered an error:\n{error_message}"
-        message = f"Subject: {subject}\n\n{text}"
-    else:
-        subject = "Tableau Batch Print - Success"
-        text = f"The Student Profile Batch Printer created {count} profiles for {school}"
-        message = f"Subject: {subject}\n\n{text}"
-    server.sendmail(GMAIL_USER, SLACK_EMAIL, message)
-    server.quit()
+class Mailer:
+    """
+    A mail server connection object for sending email notifications.
+
+    Params:
+        jobname:    The name of the job to be referenced in the emails.
+    """
+
+    def __init__(self, jobname):
+        self.jobname = jobname
+        self.mg_api_key = os.getenv("MG_API_KEY")
+        self.mg_domain = os.getenv("MG_DOMAIN")
+        self.mg_api_url = os.getenv("MG_API_URL")
+        self.from_address = os.getenv("SENDER_EMAIL")
+        self.to_address = os.getenv("RECIPIENT_EMAIL")
+
+    def _subject_line(self):
+        """Return formatted subject line based on error message content"""
+        subject_type = "Error" if self.error_message else "Success"
+        return f"{self.jobname} - {subject_type}"
+
+    def _body_text(self):
+        """Return formatted body text based on error message content."""
+        if self.error_message:
+            return f"{self.jobname} encountered an error.\n{self.error_message}"
+        else:
+            return f"{self.jobname} completed successfully."
+
+    def _attachments(self):
+        """Return list of attachments (in this case, logs)"""
+        filename = "app.log"
+        if os.path.exists(filename):
+            return [("attachment", (filename, open(filename, "rb").read()))]
+
+    def notify(self, error_message=None):
+        """Send email success/error notifications using Mailgun API."""
+        self.error_message = error_message
+        requests.post(
+            f"{self.mg_api_url}{self.mg_domain}/messages",
+            auth=("api", self.mg_api_key),
+            files=self._attachments(),
+            data={
+                "from": self.from_address,
+                "to": self.to_address,
+                "subject": self._subject_line(),
+                "text": self._body_text(),
+            },
+        )
