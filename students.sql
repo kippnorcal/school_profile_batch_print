31785a0662c96fa798e03d61a86d1109e1dc4c7a
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2021-11-02 15:41:18 -0700

    PROJ22-779 Add grade level filter for lkschools

--- students.sql
+++ students.sql
@@ -6,6 +6,7 @@ SELECT
 FROM custom.vw_enrollment_course_section_ps cs
 LEFT JOIN custom.lkSchools lks
     ON cs.schoolid_record = lks.SystemSchoolID
+    AND cs.CurrGradeLevel BETWEEN lks.LowGrade AND lks.HighGrade
 WHERE cs.Schoolyear4digit = custom.fn_SchoolYear4Digit (GETDATE())
     AND cs.seq_latestEnrollment = 1
     AND cs.subject = 'HR'
